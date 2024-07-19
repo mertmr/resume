74eb010b138a26902fbb079ef1e489dce1de2088
mert.meral <mert.meral@emirates.com>
2024-07-19 08:35:41 +0400

    fix try playwright 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+      devToolbar: {
+        enabled: false
+      },
   integrations: [tailwind()],
   vite: {
     ssr: {
